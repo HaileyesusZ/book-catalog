68d544cac6fdff97b62ede01d57cf38690422553
Haileyesus Zemed <rebbet21@gmail.com>
2021-10-13 21:01:21 +0300

    initial commit; sets up project

---
+++ README.md
@@ -0,0 +1,11 @@
+
+# Book CATALOG
+
+## Tech Stack
+### Frontend
+1. React
+2. Typescript
+
+### Backend
+1. Express 
+2. Typescript
---
+++ backend-api/src/server.ts
@@ -0,0 +1,9 @@
+import express from 'express'
+
+const app = express()
+app?.use()
+const PORT = 3000
+
+app.listen(PORT, () => {
+    console.log('app running')
+})
